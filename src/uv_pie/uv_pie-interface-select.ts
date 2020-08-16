1f013efd210f5fc2bb214d6ba79703c0687fbf90
Yuvraj Patil <5191208+yuvi1422@users.noreply.github.com>
2020-08-16 13:58:22 +0530

    Used UVAction interface instead of UVPieSelect.

--- src/uv_pie/uv_pie-interface-select.ts
+++
@@ -1,7 +0,0 @@
-import UVItem from '../uv_interface-item';
-
-export default interface UVPieSelect {
-  type: string;
-  sliceIndex: number;
-  data: UVItem[];
-};
--- src/uv_pie/uv_pie-saga.ts
+++ src/uv_pie/uv_pie-saga.ts
@@ -1,12 +1,12 @@
 import { takeEvery, put } from 'redux-saga/effects';
 import UV_PIE from './uv_pie-constants';
 import { updateBarChart } from '../uv_bar_chart/uv_bar_chart-actions';
-import UVPieSelect from './uv_pie-interface-select';
+import UVAction from '../uv_interface-action';
 
-function* selectSlice(pieSlice: UVPieSelect) {
+function* selectSlice(pieSlice: UVAction) {
   // Note: We can dispatch event using store dispatch but it's not recommended in generator by Redux Saga docs.
   // put is most testable than store dispatch
-  yield put(updateBarChart(pieSlice.sliceIndex, pieSlice.data));
+  yield put(updateBarChart(pieSlice.config.index, pieSlice.data));
 }
 
 export function* uvPieSaga() {
