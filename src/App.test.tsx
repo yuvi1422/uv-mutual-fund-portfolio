dd357991a0bfae8557e5662c95814703e9d8c628
Yuvraj Patil <5191208+yuvi1422@users.noreply.github.com>
2021-01-04 10:04:45 +0530

    Added Test cases for App component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import App from './App';
-import Header from './components/uv_header/uv_header';
+import UVHeader from './components/uv_header/uv_header';
+
+import * as headerJSONData from './components/uv_header/uv_header.json'
 
 describe('App Component', ()=> {
   it('renders without crashing', ()=> {
     shallow(<App />);
   });
   it('Contains 1 Header component', ()=> {
-    var wrapper = shallow(<App />);
-    expect(wrapper.find(Header)).toHaveLength(1);
+    const shallowWrapper = shallow(<App />);
+    const headerComponent = shallowWrapper.find(UVHeader);
+    expect(headerComponent).toHaveLength(1);
+
+    const deepWrapper = mount(<App />);
+    expect(deepWrapper.find(UVHeader).prop('data')).toEqual(headerJSONData.config);
   });
 })
 
